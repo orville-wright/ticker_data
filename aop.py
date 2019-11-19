8934c2571237ce445a434e8b88aa355a251cbc34
Orville Wright <orville.wright@yahoo.com>
2019-11-18 17:31:59 -0800

    Modernize code

--- aop.py
+++ aop.py
@@ -6,86 +6,106 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 import re
-#from tabulate import tabulate 
-
-with urllib.request.urlopen("https://finance.yahoo.com/gainers/") as url:
-    s = url.read()
-    soup = BeautifulSoup(s, "html.parser")
+import logging
+import argparse
 
 
-# ATTR style search. Results -> Dict
-# <tr tag in target merkup line has a very complex 'class=' but the attributes are unique. e.g. 'simpTblRow' is just one unique attribute
-all_tag_tr = soup.find_all(attrs={"class": "simpTblRow"})
+# logging setup
+logging.basicConfig(level=logging.INFO)
 
-# This is the target markup line I am scanning for...
-# soup.find_all( "tr", class_="simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc($extraLightBlue)" )
+def get_topgainers():
+    with urllib.request.urlopen("https://finance.yahoo.com/gainers/") as url:
+        s = url.read()
+        soup = BeautifulSoup(s, "html.parser")
 
-# Example CSS Selector
-#all_tag_tr1 = soup.select( "tr.simpTblRow.Bgc" )
+    # ATTR style search. Results -> Dict
+    # <tr tag in target merkup line has a very complex 'class=' but the attributes are unique. e.g. 'simpTblRow' is just one unique attribute
+    all_tag_tr = soup.find_all(attrs={"class": "simpTblRow"})
 
-# create an empty pandas DataFrame with specific column names pre-defined
+    # target markup line I am scanning looks like this...
+    # soup.find_all( "tr", class_="simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc($extraLightBlue)" )
 
-top_gainers = pd.DataFrame(columns=[ 'Row', 'Symbol', 'Co_name', 'Cur_price', 'Prc_change', 'Pct_change'] )
+    # Example CSS Selector
+    #all_tag_tr1 = soup.select( "tr.simpTblRow.Bgc" )
 
-x = 1    # row counter Also leveraged for unique dataframe key
-m = 20
-for datarow in all_tag_tr:
+    # create empty pandas DataFrame with specific column names pre-defined
+    top_gainers = pd.DataFrame(columns=[ 'Row', 'Symbol', 'Co_name', 'Cur_price', 'Prc_change', 'Pct_change'] )
 
-    """ 
-    if x % m == 0:
+    x = 1    # row counter Also leveraged for unique dataframe key
+    m = 20
+    for datarow in all_tag_tr:
+        # 1st <td> cell : ticker symbol info & has comment of company name
+        # 2nd <td> cell : company name
+        # 3rd <td> cell : price
+        # 4th <td> cell : $ change
+        # 5th <td> cell : % change
+        # more cells in <tr> data row...but I'm not interested in them at moment.
+
+        # BS4 generator object comes from "extracted strings" BS4 operation (nice)
+        extr_strings = datarow.stripped_strings
+        co_sym = next(extr_strings)
+        co_name = next(extr_strings)
+        price = next(extr_strings)
+        change = next(extr_strings)
+        pct = next(extr_strings)
+
+        co_sym_lj = np.char.ljust(co_sym, 6)       # use numpy to left justify TXT in pandas DF
+        co_name_lj = np.char.ljust(co_name, 20)    # use numpy to left justify TXT in pandas DF
+
+        # note: Pandas DataFrame : top_gainers pre-initalized as EMPYT
+        # Data treatment:
+        # Data is extracted as raw strings, so needs wrangeling...
+        #    price - stip out any thousand "," seperators and cast as true decimal via numpy
+        #    change - strip out chars '+' and ',' and cast as true decimal via numpy
+        #    pct - strip out chars '+ and %' and cast as true decimal via numpy
+        ds_data0 = [[ \
+                   x, \
+                   co_sym_lj, \
+                   co_name_lj, \
+                   np.float(re.sub('\,', '', price)), \
+                   np.float(re.sub('[\+,]', '', change)), \
+                   np.float(re.sub('[\+%]', '', pct)) ]]
+
+        df_temp0 = pd.DataFrame(ds_data0, columns=[ 'Row', 'Symbol', 'Co_name', 'Cur_price', 'Prc_change', 'Pct_change' ], index=[x] )
+        top_gainers = top_gainers.append(df_temp0)    # append this ROW of data into the DataFrame
+        x+=1
+
+    return top_gainers
+
+# Hacking function - keep me arround for a while
+def working(self, x, y):
+    """simple progress dialogue function"""
+    if x % y == 0:
         print ( " " )
     else:
         print ( ".", end="" )
-    """
+    return
 
-    # 1st <td> cell : ticker symbol info & has comment of company name
-    # 2nd <td> cell : company name
-    # 3rd <td> cell : price
-    # 4th <td> cell : $ change
-    # 5th <td> cell : % change
-    # more cells in <tr> data row...but I'm not interested in them at moment.
+# main() now...
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v','--verbose', help='verbose error logging', action='store_true', dest='bool_verbose', required=False, default=False)
 
-    #ticker = datarow.a.get_text()
-    #co_name = datarow.a['title']
+    args = vars(parser.parse_args())
+    print ( " " )
+    print ( "########## Initalizing ##########" )
+    print ( " " )
 
-    # These are the items that I am intersted in...
+    # ARGS[] pre-processing - set-ip logging before anything else
+    if args['bool_verbose'] is True:
+        print ( "Enabeling verbose info logging..." )
+        logging.disable(0)     # Log level = NOTSET
+        print ( "All command line args..." )
+        print ( parser.parse_args() )
+        print ( " " )
+    else:
+        logging.disable(20)    # Log lvel = INFO
 
-    # BS4 generator object on "extracted strings"
-    extr_strings = datarow.stripped_strings
+    stock_topgainers = get_topgainers()
+    pd.set_option('display.max_rows', None)
+    pd.set_option('max_colwidth', 30)
+    print ( stock_topgainers.sort_values(by='Pct_change', ascending=False ) )    # only do after fixtures datascience dataframe has been built
 
-    co_sym = next(extr_strings)
-    co_name = next(extr_strings)
-    price = next(extr_strings)
-    change = next(extr_strings)
-    pct = next(extr_strings)
 
-    co_sym_lj = np.char.ljust(co_sym, 6)       # use numpy to left justify TXT in pandas DF
-    co_name_lj = np.char.ljust(co_name, 20)    # use numpy to left justify TXT in pandas DF
-
-    # note: Pandas DataFrame : top_gainers pre-initalized as EMPYT on __init__
-    # Data treatment:
-    # Data is extracted as raw strings, so needs wrangeling...
-    #    price - stip out any thousand "," seperators and cast as true decimal via numpy
-    #    change - stripped of chars '+' and ',' and cast as true decimal via numpy 
-    #    pct is stripped of chars '+ and %' and cast as true decimal via numpy
-    ds_data0 = [[ \
-               x, \
-               co_sym_lj, \
-               co_name_lj, \
-               np.float(re.sub('\,', '', price)), \
-               np.float(re.sub('[\+,]', '', change)), \
-               #re.sub('\+', '#', change), \
-               np.float(re.sub('[\+%]', '', pct)) ]]
-
-    df_temp0 = pd.DataFrame(ds_data0, columns=[ 'Row', 'Symbol', 'Co_name', 'Cur_price', 'Prc_change', 'Pct_change' ], index=[x] )
-
-    top_gainers = top_gainers.append(df_temp0)    # append this ROW of data into the DataFrame
-
-    x+=1
-
-pd.set_option('display.max_rows', None)
-pd.set_option('max_colwidth', 30)
-
-print ( top_gainers.sort_values(by='Pct_change', ascending=False ) )    # only do after fixtures datascience dataframe has been built
-#print ( tabulate(top_gainers, showindex=False, headers=top_gainers.columns ) )    # only do after fixtures datascience dataframe has been built
-
+if __name__ == '__main__':
+    main()
