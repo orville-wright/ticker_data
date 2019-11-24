5a08fd7217ee88775ea3ffd30721b967f44c0dde
Orville Wright <orville.wrightt@gmail.com>
2019-11-23 20:07:01 -0800

    Hack testing thread event patterns

--- thread_event_pattern2.py
+++ thread_event_pattern2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Example code for a good/healthy Threaded wait pattern
 
-from random import random
+import random
 import threading
 import time
 
@@ -11,21 +11,22 @@ result = None
 result_available = threading.Event()
 
 def background_calculation():
-    # here goes some long calculation
+    print ( "Init bkgrnd()..." )
     global progress
     global sleeper
     for i in range(20):
-        sleeper = random() * 3
+        #sleeper = random() * 3
+        sleeper = random.randint(1, 10)
+        print ( "INSIDE bkgrnd method()", i, " - sleeping...", sleeper, "secs" )
         time.sleep( sleeper )
-        progress = i + 1
+        progress = ( (i + 1) / 20 ) * 100
 
-    # when the calculation is done, the result is stored in a global variable
+    # when the loop exits, the result is assigned, stored in a global variable & triggered
     global result
     result = 42
     result_available.set()
 
     # do some more work before exiting the thread
-    time.sleep(10)
 
 def main():
     thread = threading.Thread(target=background_calculation)
@@ -33,10 +34,11 @@ def main():
 
     # wait here for the result to be available before continuing
     while not result_available.wait(timeout=5):
+        print ( "OUTSODE bkgrnd method() - Thread waiter timed out @ 5 secs" )
         #print('\r{}% done...'.format(progress), '\r{}secs sleeping...'.format(sleeper), end='', flush=True)
-        print('\r{}% done...'.format(progress), 'sleeping {} seconds...'.format(sleeper), end='', flush=True)
+        print('\t{}% done...'.format(progress) )
 
-    print('\r{}% done...'.format(progress))
+    print( "Outside main thread waita" )
 
     print('The result is', result)
 
