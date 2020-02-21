3ddd3adc0fdb90e0c0f494fa36d1ab71679723ed
dave@keateryx <orville.wright@usakiwi.com>
2020-02-20 23:56:06 -0800

    Refine News Ai looper.

--- y_newsloop.py
+++ y_newsloop.py
@@ -22,6 +22,7 @@ class y_newsfilter:
     n_df0 = ""          # DataFrame - Full list of top gainers
     n_df1 = ""          # DataFrame - Ephemerial list of top 10 gainers. Allways overwritten
     n_df2 = ""          # DataFrame - Top 10 ever 10 secs for 60 secs
+    soup = ""           # the entire HTML doc
     ul_tag_dataset = ""      # BS4 handle of the <tr> extracted data
     inst_uid = 0
     cycle = 0           # class thread loop counter
@@ -75,71 +76,25 @@ class y_newsfilter:
         logging.info('%s - Drop all rows from DF0' % cmi_debug )
         self.n_df0.drop(self.n_df0.index, inplace=True)
         x = 1    # row counter Also leveraged for unique dataframe key
-        # full_dataset = self.ul_tag_dataset
-        #li_subset = self.ul_tag_dataset[0].li.find_all(attrs={"class": "C(#959595)"})
-        # li_subset = self.ul_tag_dataset[0].li.find_all()
+
         li_superclass = self.ul_tag_dataset.find_all(attrs={"class": "js-stream-content Pos(r)"} )
         li_subset = self.ul_tag_dataset.find_all('li')
         mini_headline = self.ul_tag_dataset.div.find_all(attrs={'class': 'C(#959595)'})
-
-        # print ( "***li TEST #1 ***" )
-        # print ( li_subset )
-        #print ( "***li TEST #2 ***" )
-        #print ( li_subset.next_element )
-
-        #li_alldataset = self.ul_tag_dataset[0].li.find_all(attrs={"class": "C(#959595)"} )
-        #print ( "***li alldataset #3 ***" )
-        #print ( li_alldataset )
+        #micro_headline = self.soup.find_all("i") #attrs={'class': 'Mx(4px)'})
 
         for datarow in range(len(li_subset)):
             html_element = li_subset[datarow]
             print ( f"================ Row: {x} ====================" )
             print ( f"News outlet: {html_element.div.find(attrs={'class': 'C(#959595)'}).string }" )
+            #print ( f"News outlet2: {datarow[0].contents}" )
+            """print ( f"DOT: {html_element.i.find(attrs={'class': 'Mx(4px)'}) }" )"""
             print ( f"News item URL: {html_element.a.get('href')}" )
             print ( f"News headline: {html_element.a.text}" )
-            #print ( f"[list item]: {x} - {html_element}" )
-
-            """
-            y = 1
-            for e in get_soup.div.next_elements:
-                # print ( f"=============== Element {y} =====================" )
-                print ( f"==== Element: {y} ====" )
-                print ( f"{e}" )
-            y += 1
-            """
+            print ( f"Brief: {html_element.p.text}" )
             x += 1
 
-                    #for datarow in self.ul_tag_dataset:
-            # BS4 generator object from "extracted strings" BS4 operation (nice)
-            #print ( f"News data row: {x} - {datarow}" )
-            #extr_strings = datarow.stripped_strings
-            #news_outlet = datarow.find_all(attrs={"class": "C(#959595)"} )         # 1st key section within this datarow
-            #news_outlet = datarow.find(attrs={"class": "C(#959595)"} )         # 1st key section within this datarow
-
-            #time_ago = datarow.next_element            # 2nd <td> : company name
-
-            # THESE WORK ...
-            # soup.div.contents[0]
-            # soup.div.string
-
-            #got_you = get_soup.string
-            # got_you = get_soup.find(data-reactid==True)
-
-            # print ( f"Cycle: {x} Data row: {my_list[datarow]}" )
-            #print ( f"Time of news: {time_ago}" )
-
-        print ( f"Mini Headline ALL: {mini_headline}" )
-        for eline in range(len(mini_headline)):
-            m = str(mini_headline[eline])
-            ms = BeautifulSoup( m, features="html.parser" )
-            print ( f"== DIV: {ms} == ")
-            print ( f"== SPAN: {ms.next_element} == ")
-
-        print ( f"li Superclass 1 only: {li_superclass}" )
-
         logging.info('%s - populated new DF0 dataset' % cmi_debug )
-        return x        # number of rows inserted into DataFrame (0 = some kind of #FAIL)
-                        # sucess = lobal class accessor (y_topgainers.n_df0) populated & updated
+        return x        # number of rows extracted
 
 # method #3
 # Hacking function - keep me arround for a while
