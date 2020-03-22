50027f01456807caedf514134cc64dbadd7b2629
dave@keateryx <orville.wright@usakiwi.com>
2020-03-22 01:00:36 -0700

    get timestamped data extraction working in Alpaca API

--- vpacos_mkt_data.py
+++ vpacos_mkt_data.py
@@ -4,6 +4,7 @@ import requests
 import alpaca_trade_api as tradeapi
 import pandas as pd
 import datetime
+import time
 
 BASEURL = 'https://paper-api.alpaca.markets'
 ACCURL = '{}/v2/account'.format(BASEURL)
@@ -79,20 +80,29 @@ def main():
     num_of_datapoints = len(data_list)
     first_open = stock_bars[0].o
     last_close = stock_bars[-1].c
+    first_time = stock_bars[0].t
+    last_time = stock_bars[-1].t
     percent_change = round( ((last_close - first_open) / first_open * 100),4 )
+    #f_realtime = time.localtime(first_time)
+    #l_realtime = time.localtime(last_time)
+
     print ( " " )
     print ( f"Now ACCESS specific data fields from the complex data structure..." )
     print ( f"---------------------------------------------------------------" )
     print ( f"FIRST OPEN price: {first_open}" )
     print ( f"LAST CLOSE price: {last_close}" )
+    print ( f"FIRST TIME: {type(first_time)} / {first_time}" )
+    print ( f"LAST TIME: {type(last_time)} / {last_time}" )
+
     print ( "CODX moved {}% in the last {} data points.".format(percent_change, num_of_datapoints))
 
     print ( " " )
     print ( f"Get and create date & time info..." )
     print ( f"---------------------------------------------------------------" )
 
+
     dt_now = datetime.datetime.now()
-    dt_now_iso = dt_now.isoformat(timespec='seconds')
+    dt_now_iso = dt_now.isoformat()
     dt_year = datetime.datetime.now().year
     dt_month = datetime.datetime.now().month
     dt_day = datetime.datetime.now().day
@@ -103,18 +113,28 @@ def main():
 
     start_time = datetime.datetime(year=dt_year, month=dt_month, day=dt_day, hour=9, minute=30, second=00).isoformat(timespec='seconds')
     end_time = datetime.datetime(year=dt_year, month=dt_month, day=dt_day, hour=9, minute=32, second=00).isoformat(timespec='seconds')
+    p_start_time = pd.Timestamp(year=dt_year, month=dt_month, day=dt_day, hour=9, minute=40, second=00).isoformat()
+
 
     print ( "YEAR: {} MONTH: {} DAY: {} HOURS: {} MINUTES: {} SECONDS: {} TZ: {}".format(dt_year, dt_month, dt_day, dt_hour, dt_min, dt_sec, dt_tz) )
     #start_time = datetime.datetime(year=dt_year, month=dt_month, day=dt_day)
     print ( f"** ISO formatted date/time now: {dt_now_iso}" )
-    print ( f"** Target START_TIME: {start_time}" )
-    print ( f"** Target END: {end_time}" )
+    print ( f"** Target START_TIME: {start_time+'-05:00'}" )
+    print ( f"** Target END: {end_time+'-05:00'}" )
+    print ( f"** pd.START TIME: {p_start_time}" )
     print ( "  " )
 
-    uber_price_info = rx.get_barset(symbols='UBER', timeframe='minute' )
-    #uber_price_info = rx.get_barset(symbols='UBER', timeframe='1Min' )
+    uber_price_info = rx.get_barset(symbols='UBER', timeframe='1Min', start='2020-03-19T09:30:00-04:00', end='2020-03-19T09:35:00-04:00', limit=10 )
     uber_stock_bars = uber_price_info['UBER']
-    print ( f"{uber_price_info}" )
+    uber_first_time = uber_stock_bars[0].t
+    uber_last_time = uber_stock_bars[-1].t
+    this_sym = uber_price_info.keys()
+    symb = list(this_sym)
+
+    print ( )
+    print ( "{} - START TIME: {}".format(symb[0], uber_first_time) )
+    print ( "{} - END TIME: {}".format(symb[0], uber_last_time) )
+    #print ( f"{uber_price_info}" )
     print ( " " )
     #ts = pd.Timestamp(year = 2009, month = 5, day = 31, hour = 4, second = 49)
     #print ( f"*** Timestamp test: {ts}" )
