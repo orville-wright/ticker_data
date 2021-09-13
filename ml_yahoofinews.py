5fd78dacb7b64c658b6891794eb0ac6255dbf34d
orville-wright <orville.wright@yahoo.com>
2021-09-13 11:43:33 -0700

    hacking

--- ml_yahoofinews.py
+++ ml_yahoofinews.py
@@ -414,10 +414,10 @@ class yfnews_reader:
 
             print ( f">>DEBUG<< rem_news type:       {type(rem_news)}" )
             print ( f">>DEBUG<< local_news type:     {type(local_news)}" )
-            print ( f">>DEBUG<< local_optrader type: {type(local_optrader)}" )
-            if type(rem_news) == type(None): print ( f"NO caas-readmore tag" )
-            if type(local_news) == type(None): print ( f"NO caas-body tag" )
-            if type(local_optrader) == type(None): print ( f"NO caas-button tag" )
+            print ( f">>DEBUG<< local_optrader type: {type(local_optrader)} / {local_optrader}" )
+            if type(rem_news) != type(None): print ( f"NO caas-readmore tag" )
+            if type(local_news) != type(None): print ( f"caas-body tag: {local_news.text}" )
+            if type(local_optrader) != type(None): print ( f"caas-button tag: {local_optrader.text}" )
 
             #
             # sa far - 3 types of possible news artciels
