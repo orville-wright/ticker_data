35de6565beba409cddd49af52efd521567a9bea5
orville-wright <orville.wright@yahoo.com>
2021-09-16 12:32:36 -0700

    typo

--- aop.py
+++ aop.py
@@ -413,7 +413,7 @@ def main():
 
         # hints are embeded in the yahoo.com URL path, but aren't authourtative or definative. They're just a HINT.
         # e.g.  https://finance.yahoo.com/video/disney-release-rest-2021-films-210318469.html
-        nt_hint_code = { 'm': ('Remote News article', 0), 'news': ('Local news article', 1), 'video': ('Local Video article', 2), 'ERROR': ('URL is garbled', 10) }
+        nt_hint_code = { 'm': ('Remote News article', 0), 'news': ('Local news article', 1), 'video': ('Local Video article', 2), 'ERROR': ('ERROR URL mangled', 10) }
 
         def hint_decoder(url):
             t_url = urlparse(sn_row['url'])
@@ -430,12 +430,12 @@ def main():
         for sn_idx, sn_row in yfn.ml_ingest.items():
             print( f"News article: {sn_idx} / eval... ", end="" )
             if sn_row['type'] == 0:                # inferred from Depth 0
-                hint = hint_decoder(sn_row, default=ERROR)        # get HINT from url found at depth 0
+                hint = hint_decoder(sn_row, default=10)        # get HINT from url found at depth 0
                 print ( f"Real news NLP candidate" )         # all type 0 are assumed to be REAL news
                 status, rem_url = yfn.get_locality(sn_idx, sn_row['symbol'], sn_row['url'])    # go deep now, with HINT
                 article_header(status, rem_url, sn_row['url'] )
             elif sn_row['type'] == 1:             # possibly not news? (Micro Ad)
-                hint = hint_decoder(sn_row, default=ERROR)       # get HINT from url found at depth 0
+                hint = hint_decoder(sn_row, default=10)       # get HINT from url found at depth 0
                 if hint == 0 or hint == 1 or hint == 2:
                     print ( f"Micro ad NLP candidate" )
                     status, rem_url = yfn.get_locality(sn_idx, sn_row['symbol'], sn_row['url'])    # go deep now!
