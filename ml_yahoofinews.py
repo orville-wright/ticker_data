1cdf9e19010a10aa7d437014b65679761b7bade5
orville-wright <orville.wright@yahoo.com>
2021-09-22 20:34:38 -0700

    brute force debugging

--- ml_yahoofinews.py
+++ ml_yahoofinews.py
@@ -511,6 +511,7 @@ class yfnews_reader:
             #print ( f">>DEBUG<< URL hint: {uhint} / Page Type hint: {thint}" )
             #if uhint == 0 or uhint == 1.0:
             #    print ( f">>DEBUG<< URL hint: {uhint} / Page Type hint: {thint}" )
+            print ( f">>DEBUG<< rem_news: {rem_news}" )
             if type(rem_news) != type(None):               # page has valid structure
                 logging.info ( f"%s - Depth: 2 / Stub-page is valid..." % cmi_debug )
                 if rem_news.find('a'):                     # BAD, no <a> zone in page or article is a REAL remote URL already
