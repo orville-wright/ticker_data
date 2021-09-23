27c5f1aeee9152fc8f1e3a72a7fbf91923f7e6a3
orville-wright <orville.wright@yahoo.com>
2021-09-23 16:28:15 -0700

    finesse dump_ml_ingest

--- ml_yahoofinews.py
+++ ml_yahoofinews.py
@@ -644,9 +644,11 @@ class yfnews_reader:
         logging.info('%s - IN' % cmi_debug )
         print ( "================= ML Ingested Depth 1 NLP candidates ==================" )
         for k, d in self.ml_ingest.items():
+            print ( f"{k:03} {d['symbol']:.5} / {d['urlhash']} Hints: t:{d['type']} u:{d['uhint']} h:{d['thint']}]" )
             if 'exturl' in d.keys():
-                print ( f"{k:03} {d['symbol']:.5} / {d['urlhash']} Hints: t:{d['type']} u:{d['uhint']} h:{d['thint']}] Local: {d['url']} Extrnl: {d['xturl']}" )
+                print ( f"{k:03} {d['symbol']:.5} / Local:    {d['url']}"
+                print ( f"{k:03} {d['symbol']:.5} / External: {d['exturl']}" )
             else:
-                print ( f"{k:03} {d['symbol']:.5} / {d['urlhash']} Hints: t:{d['type']} u:{d['uhint']} h:{d['thint']}] Local: {d['url']}" )
+                print ( f"{k:03} {d['symbol']:.5} / Local:    {d['url']}"
 
         return
