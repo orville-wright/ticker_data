3a5f1b221689afc183e15c8eb0ff667c255caf52
orville-wright <orville.wright@yahoo.com>
2021-10-02 14:09:05 -0700

    getting tricky

--- nasdaq_quotes.py
+++ nasdaq_quotes.py
@@ -235,9 +235,9 @@ class nquote:
             null_errors = 0
             for i in jd_20:
                 try:
-                    y = x[i]
+                    y = x['i']
                 except TypeError:
-                    print ( f"probe found NULL json data @: {x}['{i}']'" )
+                    print ( f"probe found NULL json data @: {(re.sub(' self.quote_', '', x)}['{i}']" )
                     null_errors += 1
                 else:
                     pass
