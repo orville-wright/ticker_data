034c657f251a413e403b04db37190eda2e6d1351
orville-wright <orville.wright@yahoo.com>
2021-10-04 14:39:15 -0700

    tiny finesse

--- aop.py
+++ aop.py
@@ -256,12 +256,12 @@ def main():
                 nq.quote.clear()               # make sure ephemerial quote{} is always empty before bailing out
                 wrangle_errors = 1
                 unfixable_errors += 1
-                print ( f"! {wrangle_errors} ", end="" )
+                print ( f"!", end="" )
                 # print ( f"- UNFIXABLE data problem / Not regular stock / ETF Trust: EF / Data issues: {wrangle_errors}" )
                 # set default data for non-regualr stocks
                 x.combo_df.at[x.combo_df[x.combo_df['Symbol'] == xsymbol].index, 'Mkt_cap'] = round(float(0), 3)
                 x.combo_df.at[x.combo_df[x.combo_df['Symbol'] == xsymbol].index, 'M_B'] = 'EF'
-                print ( f"++ {wrangle_errors}", end="" )
+                print ( f"++ {wrangle_errors} / ", end="" )
             #
             elif nq.quote['mkt_cap'] != 0:            # catch zero mkt cap
                 # insert missing data into dataframe @ row / column
@@ -278,7 +278,7 @@ def main():
                         x.combo_df.at[x.combo_df[x.combo_df['Symbol'] == xsymbol].index, 'M_B'] = i[0]
                         #print ( f"/ Mkt cap scale: {i[0]} - Data issues: {wrangle_errors}" )
                         logging.info( f"%s - Computed Market cap scale as {i[0]} / DF updated!" % cmi_debug )
-                        print ( f"+ {wrangle_errors}", end="" )
+                        print ( f"+ {wrangle_errors} / ", end="" )
                         cleansed_errors += 1
                         break
             else:
@@ -286,7 +286,7 @@ def main():
                 #print ( f"- INSERT missing data / Market cap: 0 / Mkt cap scale: UZ - Data issues: {wrangle_errors}" )
                 x.combo_df.at[x.combo_df[x.combo_df['Symbol'] == xsymbol].index, 'M_B'] = "UZ"
                 x.combo_df.at[x.combo_df[x.combo_df['Symbol'] == xsymbol].index, 'Mkt_cap'] = float(0)
-                print ( f"++ {wrangle_errors} ", end="" )
+                print ( f"++ {wrangle_errors} / ", end="" )
                 cleansed_errors += 1
 
             logging.info( f"main::x.combo ================ end quote: {qsymbol} : %s ====================" % loop_count )
