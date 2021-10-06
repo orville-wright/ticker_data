260c26db1b401ae511c5b7ea575087009b38be97
orville-wright <orville.wright@yahoo.com>
2021-10-06 10:46:16 -0700

    wtf

--- screener_dg1.py
+++ screener_dg1.py
@@ -82,10 +82,7 @@ class screener_dg1:
         x = 1                               # row counter leveraged for unique dataframe key
         print ( f">>>DEBUG<<< : td_rows: {self.td_tag_rows}" )
         for datarow in self.all_tag_tr:                  # BS4 generator object (nice, but has BS4 accessiblity limits)
-            for i in datarow.descendants:
-                print ( f">>>DEBUG<<< : {i}" )
-
-            extr_strs = datarow.children.strings
+            extr_strs = datarow.td.strings
             #extr_strs = datarow.strings
             print ( f">>>DEBUG<<< : extr_strs 1: {next(extr_strs)}" )
             print ( f">>>DEBUG<<< : extr_strs 2: {next(extr_strs)}" )
