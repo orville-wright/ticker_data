72543c6d2c8745357fd0f4c3f31b1366a6ab132f
orville-wright <orville.wright@yahoo.com>
2021-10-06 14:02:00 -0700

    more nope

--- aop.py
+++ aop.py
@@ -290,11 +290,11 @@ def main():
                 print ( f"+", end="" )
                 cleansed_errors += 1
                 if nq.asset_class == "stocks":
-                    logging.info( f"%s - Compute Mkt_cap scale tag: [ {nq.quote['mkt_cap']} ]..." % cmi_debug )
+                    logging.info( f"%s - Compute Mkt_cap scale tag: [ {nq.quote['mkt_cap']} ] / {type(nq.quote['mkt_cap'])}..." % cmi_debug )
                     for i in (("MT", 999999), ("LB", 10000), ("SB", 2000), ("LM", 500), ("SM", 50), ("TM", 10), ("UZ", 0)):
                         if i[1] >= nq.quote['mkt_cap']:
                             pass
-                        elif int(nq.quote['mkt_cap']) == int(0):
+                        elif nq.quote['mkt_cap'] == "0.0":
                             x.combo_df.at[x.combo_df[x.combo_df['Symbol'] == xsymbol].index, 'M_B'] = "UZ"
                             logging.info( f"%s - Market cap: {nq.quote['mkt_cap']} scale set to: UZ" % cmi_debug )
                             break
