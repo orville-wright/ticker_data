72afdd2808a60a8c3f36cfbef8eba9c8fa8e37e2
orville-wright <orville.wright@yahoo.com>
2021-10-05 23:13:39 -0700

    maybe

--- nasdaq_quotes.py
+++ nasdaq_quotes.py
@@ -426,8 +426,11 @@ class nquote:
                 prev_close = jsondata10['PreviousClose']['value']                   # e,g, "$138.93"
                 mkt_cap = jsondata10['MarketCap']['value']                          # e.g. "128,460,592,862"
                 today_hilo = jsondata10['TodayHighLow']['value']                    # WARN: multi-field string needs splitting/wrangeling e.g. "$143.97/$140.37"
-                avg_vol = jsondata10['AverageVolume']['value']                      # e.g. "4,811,121" or N/A
-                oneyear_target = jsondata10['OneYrTarget']['value']                 # e.g. "$151.00"
+                #
+                if self.asset_class == "stocks":
+                    avg_vol = jsondata10['AverageVolume']['value']                      # e.g. "4,811,121" or N/A
+                    oneyear_target = jsondata10['OneYrTarget']['value']                 # e.g. "$151.00"
+                #
                 beta = jsondata10['Beta']['value']                                  # e.g. 1.23
                 LII_week_hilo = jsondata10['FiftTwoWeekHighLow']['value']           # WARN: multi-field string needs splitting/wrangeling e.g. "$152.84/$105.92"
                 logging.info( '%s - Stage #3 / [7] fields - Done' % cmi_debug )
