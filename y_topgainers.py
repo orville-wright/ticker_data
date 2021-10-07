304ca0ca749639c9975f3f6f716219f83ce426b0
orville-wright <orville.wright@yahoo.com>
2021-10-07 09:52:31 -0700

    ho hum...

--- y_topgainers.py
+++ y_topgainers.py
@@ -77,7 +77,7 @@ class y_topgainers:
         x = 0      # OLD : x = 1    # row counter Also leveraged for unique dataframe key
         for j in self.tag_tbody.find_all('tr'):
             y = 1
-            for i in j.find'td'):
+            for i in j.find('td'):
                 print ( f"Data {y}: {i.contents}" )
                 # logging.info( f'%s - Data: {j.td.strings}' % cmi_debug )
             print ( f"==============================================" )
