c0d1d777020a71a5afb761bb2e6320796551e01e
orville-wright <orville.wright@yahoo.com>
2021-10-07 09:03:23 -0700

    atom editos tabs issue

--- y_topgainers.py
+++ y_topgainers.py
@@ -71,7 +71,7 @@ class y_topgainers:
         time_now = time.strftime("%H:%M:%S", time.localtime() )
         logging.info('%s - Drop all rows from DF0' % cmi_debug )
         self.tg_df0.drop(self.tg_df0.index, inplace=True)
-    	x = 0      # OLD : x = 1    # row counter Also leveraged for unique dataframe key
+        x = 0      # OLD : x = 1    # row counter Also leveraged for unique dataframe key
     	for j in self.tag_tbody.find_all('tr'):
     		extr_strs = j.strings
     		co_sym = next(extr_strs)         # 1 : ticker symbol info / e.g "NWAU"
