764b75bf61d63d01eac3fe6238525d72f1f77144
orville-wright <orville.wright@yahoo.com>
2021-10-07 10:43:51 -0700

    OMFG

--- y_topgainers.py
+++ y_topgainers.py
@@ -75,6 +75,7 @@ class y_topgainers:
         logging.info('%s - Drop all rows from DF0' % cmi_debug )
         self.tg_df0.drop(self.tg_df0.index, inplace=True)
         x = 0      # OLD : x = 1    # row counter Also leveraged for unique dataframe key
+        print ( f"===== {len(self.tag_tbody.find_all('tr'))} =================" )
         for j in self.tag_tbody.find_all('tr'):
             y = 1
             for i in j.find_all('td'):
