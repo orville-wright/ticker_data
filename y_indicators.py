909dc8553958b6b649515a18954ba3b82cee71fe
orville-wright <orville.wright@yahoo.com>
2021-10-09 15:55:47 -0700

    enable te_zone html dump debug

--- y_indicators.py
+++ y_indicators.py
@@ -93,8 +93,8 @@ class y_techevents:
             #self.te_jsondata0 = json.loads(self.te_resp0.text)
         #
         #self.te_zone = self.soup.find(attrs={"data-test": "tch-evnts"} )
-        self.te_zone = self.soup.find(attrs={"class": "D(ib) Va(m)"} )
-        #print ( f">>>DEBUG<<< : te_zone :\n{self.te_zone}" )
+        self.te_zone = self.soup.find_all(attrs={"class": "D(ib) Va(m)"} )
+        print ( f">>>DEBUG<<< : te_zone :\n{self.te_zone}" )
         self.te_short = self.te_zone.find(attrs={"value": "short"} )
         self.te_mid = self.te_zone.find(attrs={"value": "intermediate"} )
         self.te_long = self.te_zone.find(attrs={"value": "long"} )
