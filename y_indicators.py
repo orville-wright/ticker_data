c21f132b0b5a33a13287d84e3708c48617a6a499
orville-wright <orville.wright@yahoo.com>
2021-10-09 16:04:40 -0700

    different selector

--- y_indicators.py
+++ y_indicators.py
@@ -92,8 +92,8 @@ class y_techevents:
             #self.te_jsondata0 = json.loads(self.te_resp0.text)
         #
         #self.te_zone = self.soup.find(attrs={"data-test": "tch-evnts"} )
-        print ( f">>>DEBUG<<< : te_zone :\n{self.soup}" )
-        self.te_zone = self.soup.find(attrs={"class": "D(ib) Va(m)"} )
+        #self.te_zone = self.soup.find(attrs={"class": "D(ib) Va(m)"} )
+        self.te_zone = self.soup.find_all('button')
         print ( f">>>DEBUG<<< : te_zone :\n{self.te_zone}" )
         self.te_short = self.te_zone.find(attrs={"value": "short"} )
         self.te_mid = self.te_zone.find(attrs={"value": "intermediate"} )
