335eafc3ede5fa7cc6e23030498aff0733bcca7e
orville-wright <orville.wright@yahoo.com>
2021-10-09 16:16:08 -0700

    something else

--- y_indicators.py
+++ y_indicators.py
@@ -93,7 +93,7 @@ class y_techevents:
         #
         #self.te_zone = self.soup.find('tch-evnts' )
         #self.te_zone = self.soup.find(attrs={"data-test": "tch-evnts"} )
-        self.te_zone = self.soup.find(attrs={"class": "D(ib) Va(m)"} )
+        self.te_zone = self.soup.find(attrs={"class": "Mt(10px)"} )
         #self.te_zone = self.soup.find_all('button')
         print ( f">>>DEBUG<<< : te_zone :\n{self.te_zone}" )
         self.te_short = self.te_zone.find(attrs={"value": "short"} )
