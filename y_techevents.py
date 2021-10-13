02e586a14705bf9ff0cb83eb87b552f164d3ee45
orville-wright <orville.wright@yahoo.com>
2021-10-13 15:09:31 -0700

    getting more precise

--- y_techevents.py
+++ y_techevents.py
@@ -95,7 +95,7 @@ class y_techevents:
         #self.te_zone = self.soup.find(attrs={"data-test": "tch-evnts"} )
         self.te_zone = self.soup.find(attrs={"id": "chrt-evts-mod"} )
         self.te_lizones = self.te_zone.li.find_all()
-        #self.te_zone_s = self.te_zone.li.find_all()
+        self.te_zone_title = self.te_lizones.find_all(attrs={"class": "IbBlock"} )
         #self.te_zone_i = self.soup.button.find_all()
         #self.te_zone_l = self.soup.find_all('button')
         print ( f"\n>>>DEBUG<<< : te_zone :\n{self.te_zone}" )
@@ -119,9 +119,9 @@ class y_techevents:
         logging.info( f"{cmi_debug} - Drop all rows from DF0" )
         #self.tg_df0.drop(self.tg_df0.index, inplace=True)
         x = 0   # row counter / = index_id for DataFrame
-        print ( f"===== Rows: {len(self.te_lizones)}  =================" )
+        print ( f"===== <li> zones: {len(self.te_lizones)}  =================" )
         #for j in self.te_zone.find_all('button'):
-        for j in self.te_lizones:
+        for j in self.te_zone_title:
             # >>>DEBUG<< for when yahoo.com changes data model...
             y = 1
             for i in j.strings:
