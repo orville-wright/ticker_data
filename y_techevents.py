49ea63a37db84137f19c2c762d6938387d5436a8
orville-wright <orville.wright@yahoo.com>
2021-10-13 15:02:37 -0700

    refining

--- y_techevents.py
+++ y_techevents.py
@@ -94,7 +94,7 @@ class y_techevents:
         #self.te_zone = self.soup.find('tch-evnts' )
         #self.te_zone = self.soup.find(attrs={"data-test": "tch-evnts"} )
         self.te_zone = self.soup.find(attrs={"id": "chrt-evts-mod"} )
-        self.te_zones = self.te_zone.li.find_all()
+        self.te_lizones = self.te_zone.li.find_all()
         #self.te_zone_s = self.te_zone.li.find_all()
         #self.te_zone_i = self.soup.button.find_all()
         #self.te_zone_l = self.soup.find_all('button')
@@ -119,12 +119,12 @@ class y_techevents:
         logging.info( f"{cmi_debug} - Drop all rows from DF0" )
         #self.tg_df0.drop(self.tg_df0.index, inplace=True)
         x = 0   # row counter / = index_id for DataFrame
-        print ( f"===== Rows: {len(self.te_zone)}  =================" )
+        print ( f"===== Rows: {len(self.te_lizones)}  =================" )
         #for j in self.te_zone.find_all('button'):
-        for j in self.te_zones:
+        for j in self.te_lizones.strings:
             # >>>DEBUG<< for when yahoo.com changes data model...
             y = 1
-            for i in j.descendants:
+            for i in j:
             	print ( f"Data {y}: {i}" )
             	# logging.info( f'%s - Data: {j.td.strings}' % cmi_debug )
             	y += 1
