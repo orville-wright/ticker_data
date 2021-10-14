ac6355f33a1d54742dc0d9897f1757ea566447a3
orville-wright <orville.wright@yahoo.com>
2021-10-13 17:28:29 -0700

    intra tag walking

--- y_techevents.py
+++ y_techevents.py
@@ -129,10 +129,10 @@ class y_techevents:
             for i in j:
                 #print ( f"Data {y}: {i}" )
                 print ( f"Tech Event: {i.svg}" )
-                red = i.svg.contents
-                grn = len(i.svg)
+                red = i.svg[4]
+                #grn = i.svg.parent.contents
                 print ( f"RED:   {red}" )
-                print ( f"GREEN: {grn}" )
+                #print ( f"GREEN: {grn}" )
 
                 # logging.info( f'%s - Data: {j.td.strings}' % cmi_debug )
                 y += 1
