a486b8b9233cfb384b7e865e76933ad1a9146fd6
orville-wright <orville.wright@yahoo.com>
2021-10-13 17:17:24 -0700

    ) doesnt work in find

--- y_techevents.py
+++ y_techevents.py
@@ -130,9 +130,9 @@ class y_techevents:
                 #print ( f"Data {y}: {i}" )
                 print ( f"Tech Event: {i.svg}" )
                 red = i.svg.find(class_ = "RotateZ") )
-                grn = i.svg.find(class_ = "RotateZ(180deg)" )
+                #grn = i.svg.find(class_ = "RotateZ(180deg)" )
                 print ( f"RED:   {red}" )
-                print ( f"GREEN: {grn}" )
+                #print ( f"GREEN: {grn}" )
 
                 # logging.info( f'%s - Data: {j.td.strings}' % cmi_debug )
                 y += 1
