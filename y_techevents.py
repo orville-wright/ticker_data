493943c79cad27754bfcdfa6924b7100c2188982
orville-wright <orville.wright@yahoo.com>
2021-10-13 18:09:05 -0700

    cast to string in re.search

--- y_techevents.py
+++ y_techevents.py
@@ -130,7 +130,7 @@ class y_techevents:
                 #print ( f"Data {y}: {i}" )
                 print ( f"Tech Event: {i.svg}" )
                 red = i.svg.parent.contents
-                grn = re.search('RotateZ', red)
+                grn = re.search('RotateZ', str(red) )
                 print ( f"RED:   {red}" )
                 print ( f"GREEN: {grn}" )
                 # logging.info( f'%s - Data: {j.td.strings}' % cmi_debug )
