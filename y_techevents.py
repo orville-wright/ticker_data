5224c4748ffa6a5b8370c26d2bd615f0189ce23e
orville-wright <orville.wright@yahoo.com>
2021-10-13 17:36:27 -0700

    hmmm....

--- y_techevents.py
+++ y_techevents.py
@@ -129,7 +129,7 @@ class y_techevents:
             for i in j:
                 #print ( f"Data {y}: {i}" )
                 print ( f"Tech Event: {i.svg}" )
-                red = i.svg[4]
+                red = i.svg.find(class_)
                 #grn = i.svg.parent.contents
                 print ( f"RED:   {red}" )
                 #print ( f"GREEN: {grn}" )
