205205bb12339f17c7bc8b3f292f10f5293d2ee4
orville-wright <orville.wright@yahoo.com>
2021-10-17 16:25:49 -0700

    BS

--- nasdaq_quotes.py
+++ nasdaq_quotes.py
@@ -452,7 +452,7 @@ class nquote:
                     LII_week_hilo]
 
             for kk in iter(wrangle_work):
-                    print ( f"{kk+_}: {kk}" )
+                    print ( f"{kk.repr()}: {kk}" )
                     kc += 1
 
             co_sym_lj = co_sym.strip()
