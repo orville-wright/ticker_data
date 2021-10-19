590728791ba3e778d2c086dcacf154427749f34d
orville-wright <orville.wright@yahoo.com>
2021-10-19 11:40:41 -0700

    missing self.

--- y_techevents.py
+++ y_techevents.py
@@ -212,7 +212,7 @@ class y_techevents:
         # self.te_df0.drop(self.te_df0.index, inplace=True)        # ensure the DF is empty
         logging.info( f"{cmi_debug} - Populate DF with Tech Events emphemerial dict data" )
         te_temp_df0 = pd.DataFrame(data0, columns=[ 'Symbol', 'Today', 'Short', 'Mid', 'Long', 'Time' ] )
-        te_df0 = te_df0.append(te_temp_df0, ignore_index=True)
+        self.te_df0 = self.te_df0.append(te_temp_df0, ignore_index=True)
         logging.info( f"{cmi_debug} - Tech Event DF created" )
 
         return
