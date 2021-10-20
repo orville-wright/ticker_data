bd039a69f4cd096b493718125c9e636c624639e3
orville-wright <orville.wright@yahoo.com>
2021-10-19 19:16:08 -0700

    optimize

--- aop.py
+++ aop.py
@@ -337,7 +337,7 @@ def main():
         print ( f"\n===== Tech Events performance Sentiment ==============================" )
         #print ( f"{te.te_df0}" )
         #mask = {'Today': ['Bullish'], 'Short': ['Bullish'], 'Mid': ['Bullish'], 'Long': ['Bullish']}
-        print ( f"{te.te_df0.isin({'Today': ['Bullish'], 'Short': ['Bullish'], 'Mid': ['Bullish'], 'Long': ['Bullish']}).all()}" )
+        print ( f"{te.te_df0.isin({'Today': ['Bullish'], 'Short': ['Bullish'], 'Mid': ['Bullish'], 'Long': ['Bullish']})}" )
         #print ( f"{te.te_df0.isin('mask')}" )
         #hot_result = (te.te_df0['Today', 'Short', 'Mid', 'Long'] == 'Bullish')
         #print ( f"{hot_result}" )
