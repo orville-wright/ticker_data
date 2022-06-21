9568a93ceaae5a081810c90ce52e829cde76d863
Orville Wright <orville.wright@yahoo.com>
2022-06-21 10:27:29 -0700

    slow going today

--- aop.py
+++ aop.py
@@ -326,7 +326,7 @@ def main():
         #
         # HACKING : show uniques from COMBO def
         print ( f"***** Hacking ***** " )
-        print ( f"{x.unique_symbols[:,'Symbol']}" )
+        print ( f"{x.unique_symbols.loc[:,'Symbol']}" )
     else:
         pass
 
