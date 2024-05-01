8162bf23e06933b249e3447eb0fbe3e3aa36b7fd
Orville Wright <orville.wright@yahoo.com>
2024-04-30 19:15:37 -0700

    hunt : (z_float) bug

--- shallow_logic.py
+++ shallow_logic.py
@@ -195,12 +195,12 @@ class combo_logic:
                 # this needs to be fixed
                 print ( f">>>>>>>>>>>>>>>>>>>>> DEBUG 2 <<<<<<<<<<<<<<<<<<<<<" )
                 logging.info( f"{cmi_debug} - Set {wq.asset_class} Mkt_cap to: {wq.qd_quote['mkt_cap']}" )
-                z_float = round(float(wq.qd_quote['mkt_cap']))
+                z_float = (float(wq.qd_quote['mkt_cap']))
                 print ( f">>>>> DEBUG: {z_float} / type: {type(z_float)}" )
-                print ( f"{self.combo_df}" )
+                print ( f"{self.combo_df[self.combo_df['Symbol'] == xsymbol]}" )
                 print ( f">>>>>>>>>>>>>>>>>>>>> DEBUG 3 <<<<<<<<<<<<<<<<<<<<<" )
 
-                self.combo_df.at[self.combo_df[self.combo_df['Symbol'] == xsymbol].index, 'Mkt_cap'] = z_float
+                self.combo_df.at[self.combo_df[self.combo_df['Symbol'] == xsymbol].index, 'Mkt_cap'] = round(z_float, 3)
                 print ( f"+", end="" )
                 fixchars += 1
                 cleansed_errors += 1
