307083685dd91f8f31c3dc1ad815dae110d4813b
Orville Wright <orville.wright@yahoo.com>
2024-05-02 22:30:03 -0700

    hunt : (bug) confuised

--- aop.py
+++ aop.py
@@ -243,7 +243,7 @@ def main():
         # temp_1 = x.combo_df.sort_values(by=['Symbol'])                        # sort by sumbol name (so dupes are linearly grouped)
         # temp_1.reset_index(inplace=True, drop=True)                           # reset index
         temp_1 = x.combo_df.sort_values(by=['Pct_change'], ascending=False)     # sort by %
-        x.combo_dupes_only_listall()
+        print ( f">>> DEBUG: \n {x.combo_dupes_only_listall()}" )
         temp_2 = x.combo_listall_nodupes()                                      # dupes by SYMBOL only
         print ( " " )
         print ( "========== ** OUTLIERS ** : Unusual UP volume + Top Gainers by +5% ================================" )
