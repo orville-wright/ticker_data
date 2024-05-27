735fd9d1efa7a63a3be35842e784d64fe24a084d
Orville Wright <orville.wright@yahoo.com>
2024-05-26 19:34:09 -0700

    fix (remove % in logger) cant handel % sign

--- y_topgainers.py
+++ y_topgainers.py
@@ -115,11 +115,11 @@ class y_topgainers:
                 if (re.search('\+', pct_val)) or  (re.search('\-', pct_val)) is True:
                     logging.info( f"{cmi_debug} : {pct_val} : % CHANGE is signed [+-], stripping..." )
                     pct_cl = re.sub('[\+\-]', "", pct_val)       # remove +/- sign
-                    logging.info( f"%s : % CHANGE +/- cleaned : {pct_cl}" % cmi_debug )
+                    logging.info( f"%s : pct CHANGE +/- cleaned : {pct_cl}" % cmi_debug )
                 else:
                     logging.info( f"{cmi_debug} : {pct_val} : % CHANGE is NOT signed [+-]" )
                     pct_cl = re.sub('[\,]', "", pct_val)       # remove
-                    logging.info( f"%s : % CHANGE , cleaned : {pct_cl}" % cmi_debug )
+                    logging.info( f"%s : pct CHANGE , cleaned : {pct_cl}" % cmi_debug )
 
                 z = 0
                 pct_val = pct_sign
