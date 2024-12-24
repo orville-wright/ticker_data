c92a389cc764322f6bf25a332112406fcf67fa5d
Orville Wright <orville.wright@yahoo.com>
2024-12-24 10:35:58 -0800

    debig : (tidy up Rich Markup escaping )

--- ml_yahoofinews.py
+++ ml_yahoofinews.py
@@ -515,8 +515,8 @@ class yfnews_reader:
                 logging.info( f'%s - cache holds oject type:   {type(cy_soup)}' % cmi_debug )
                 logging.info( f'%s - Dataset holds oject type: {type(dataset_2)}' % cmi_debug )
                 print ( f"############################### debug ################################" )
-                print ( f"### DEBUG: \n {cy_soup.html}" )
-                print ( f"### DEBUG: \n {dataset_2}" )
+                print ( f"### DEBUG: {cy_soup.html}" )
+                print ( f"### DEBUG: {dataset_2}" )
                 print ( f"################################ END #################################" )
                 nsoup = BeautifulSoup(dataset_2, "html.parser")
             else:
